ac47c41b833eab36415ba39f8cc276771f902f70
Princely <mabdulwasii@gmail.com>
2022-12-28 10:23:41 +0100

    Initial commit

---
+++ src/main/java/com/indexpay/transfer/BankTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.indexpay.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransferServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTransferServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/indexpay/transfer/BankTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indexpay.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransferServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
