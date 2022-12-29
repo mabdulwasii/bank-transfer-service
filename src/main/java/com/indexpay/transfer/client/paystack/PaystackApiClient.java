2166525fd842205a89992a463fb08b471109b405
Princely <mabdulwasii@gmail.com>
2022-12-30 00:27:08 +0100

    Add some constants

--- src/main/java/com/indexpay/transfer/client/paystack/PaystackApiClient.java
+++ src/main/java/com/indexpay/transfer/client/paystack/PaystackApiClient.java
@@ -3,6 +3,7 @@ package com.indexpay.transfer.client.paystack;
 
 import com.indexpay.transfer.exception.GenericException;
 import com.indexpay.transfer.service.dto.PaystackGetBanksResponse;
+import com.indexpay.transfer.utils.AppConstants;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -11,20 +12,12 @@ import java.util.Collections;
 
 @Component
 public class PaystackApiClient {
-    private static final String AUTHORIZATION_HEADER="Authorization";
-    private static final String TOKEN_TYPE = "Bearer ";
-    public static final String ACCEPT_HEADER = "Accept";
-    public static final String APPLICATION_JSON = "application/json";
-    public static final String CONTENT_TYPE_HEADER = "Content-Type";
-
     private final PaystackConfigProperties properties;
     private final RestTemplate template;
-
     public PaystackApiClient(PaystackConfigProperties properties, RestTemplate template) {
         this.properties = properties;
         this.template = template;
     }
-
     public PaystackGetBanksResponse getBanks() {
         ResponseEntity<PaystackGetBanksResponse> response =
                 template.exchange(properties.getUrl(), HttpMethod.GET,
@@ -34,14 +27,10 @@ public class PaystackApiClient {
         }
         return response.getBody();
     }
-
-
     private HttpHeaders getHeaders() {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.add("AUTHORIZATION_HEADER", TOKEN_TYPE + properties.getSecretKey());
+        headers.add("AUTHORIZATION_HEADER", AppConstants.TOKEN_TYPE + properties.getSecretKey());
         return headers;
     }
-
-
 }
